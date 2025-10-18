2742c238fa045df846c989225a44ff7a4922067f
vanalm <jpvano@gmail.com>
2025-10-17 20:36:49 -1000

    Initial Commit

---
+++ cocobuoys/cocobuoysApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocobuoysApp.swift
+//  cocobuoys
+//
+//  Created by Jacob van Almelo on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct cocobuoysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cocobuoysTests/cocobuoysTests.swift
@@ -0,0 +1,17 @@
+//
+//  cocobuoysTests.swift
+//  cocobuoysTests
+//
+//  Created by Jacob van Almelo on 10/17/25.
+//
+
+import Testing
+@testable import cocobuoys
+
+struct cocobuoysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
